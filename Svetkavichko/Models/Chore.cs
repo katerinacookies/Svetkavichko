f382c1e97f733be1a26ba07938d5140670ca992f
katerinacookies <sokolyt@gmail.com>
2025-03-22 12:00:50 +0200

    Works rn

---
+++ Svetkavichko/Models/Chore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Svetkavichko.Models
+{
+    public class Chore
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+        public bool IsDoneToday { get; set; } = false;
+        public DateOnly DateDone {  get; set; }
+    }
+}
---
+++ Svetkavichko/Models/Music.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Svetkavichko.Models
+{
+    public class Music
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Title { get; set; }
+        [MaxLength(150)]
+        public string Artist { get; set; }
+        public string FilePath { get; set; }
+
+    }
+}
